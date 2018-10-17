2254feb76084424db6ef40524631ffe7fd3d4cca
David Wood <dwood96x@gmail.com>
2018-10-17 14:39:39 -0400

    Got to step 4

--- Key-Value Store/Program.cs
+++ Key-Value Store/Program.cs
@@ -10,6 +10,26 @@ namespace Key_Value_Store
     {
         static void Main(string[] args)
         {
+
         }
+    }
+    struct KeyValue
+    {
+        public string key { get; }
+        public object value { get; }
+        public KeyValue(string _key,object _value)
+        {
+            key = _key;
+            value = _value;
+        }
+    }
+    class MyDictionary
+    {
+        private KeyValue[] keys = new KeyValue[100];
+        private int NumOfKeys = 0;
+    }
+    class KeySystem
+    {
+        //IDK if this indexer belongs here
     }
 }
